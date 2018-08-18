e29cdea26fa14c6599e90962f258a6941aa7aa7e
Garima Singh <igarimasingh@gmail.com>
2018-08-18 16:12:22 +0800

    Gears up for release

---
+++ enlightenme/__version__.py
@@ -0,0 +1,8 @@
+__title__ = 'enlightenme'
+__description__ = 'LI for curating and reading news/updates.'
+__url__ = 'https://github.com/singhgarima/enlightenme'
+__version__ = '0.0.1'
+__author__ = 'Garima Singh'
+__author_email__ = 'igarimasingh+enlightenme@gmail.com'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2018 Garima Singh'
--- setup.py
+++ setup.py
@@ -1,14 +1,63 @@
+import json
+import os
+
 from setuptools import setup
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+about_filename = os.path.join(here, 'enlightenme', '__version__.py')
+with open(about_filename, 'r', encoding='utf-8') as f:
+    exec(f.read(), about)
+
+long_description = None
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+install_requires = []
+tests_require = []
+
+with open('Pipfile.lock') as fd:
+    lock_data = json.load(fd)
+    install_requires = [
+        package_name + package_data['version']
+        for package_name, package_data in lock_data['default'].items()
+    ]
+    tests_require = [
+        package_name + package_data['version']
+        for package_name, package_data in lock_data['develop'].items()
+    ]
+
 setup(
-    name='enlightenme',
-    version='0.1',
-    py_modules=['enlightenme'],
-    install_requires=[
-        'Click',
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    license=about['__license__'],
+
+    install_requires=install_requires,
+    tests_require=tests_require,
+
+    packages = [
+        'enlightenme'
     ],
     entry_points='''
         [console_scripts]
         enlightenme=enlightenme.enlightenme:cli
     ''',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
 )
