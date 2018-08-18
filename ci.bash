e034265d86f1b1cddc33d6b18580776fc3509bc3
Garima Singh <igarimasingh@gmail.com>
2018-08-18 17:03:54 +0800

    Switching to manual release after unable to integrate with travis deploy

--- ci.bash
+++ ci.bash
@@ -12,15 +12,31 @@ function build () {
 }
 
 function run-test () {
+    echo "Releasing the pachaage"
+    python setup.py release
+
     echo "Running the tests"
     pipenv run coverage run setup.py test
     pipenv run coverage report
+}
+
+function release () {
+    echo "Publishing to PyPI"
+    pipenv run pip install twine
+    pipenv run twine upload dist/* -u ${PYPI_USER_NAME} -p ${PYPI_PWD}
 }
 
 function main () {
     lint || return 1
     build || return 1
     run-test || return 1
+
+    if [[ $TRAVIS_PYTHON_VERSION =~ ^v3\.6+$ ]]; then
+        if [[ $TRAVIS_TAG =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+            echo "Releasing tag $TRAVIS_TAG with Python $TRAVIS_PYTHON_VERSION"
+            release || return 1
+        fi
+    fi
 }
 
 main "$@" || exit 1
