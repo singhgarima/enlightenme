6b05b41e7532f500b6effba0ea279faa87459b5c
Garima Singh <igarimasingh@gmail.com>
2018-09-22 11:05:52 +0800

    Bump version: 0.0.5 → 0.0.6

--- enlightenme/__version__.py
+++ enlightenme/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'enlightenme'
 __description__ = 'LI for curating and reading news/updates.'
 __url__ = 'https://github.com/singhgarima/enlightenme'
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 __author__ = 'Garima Singh'
 __author_email__ = 'igarimasingh+enlightenme@gmail.com'
 __license__ = 'MIT License'
